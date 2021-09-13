89c7463ec93a1e9c22a5e4ac8f22460ffc2c5948
popstar <1332381368@qq.com>
2021-09-13 22:57:34 +0800

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -17,9 +17,10 @@ public class Greeter {
   }
 
   public static void main(String[]args) {
-    String language = args[0];
-    String country = args[1];
-    String name = args[2];
+	String[] yxr={"en","NZ","yxr"};
+    String language = yxr[0];
+    String country = yxr[1];
+    String name = yxr[2];
     Greeter greeter = new Greeter(language, country, name);
     System.out.println(greeter.sayHello());
   }
